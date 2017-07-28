e77a6b2a1a40ca1b3359ffbac1284db2acce7b07
yling0 <xyun2016@gmail.com>
2017-07-28 15:04:18 -0400

    correction

--- src/invoice/Invoice.java
+++ src/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package invoice;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
 //This is the parent class. 
@@ -8,7 +9,13 @@ public class Invoice {
 		
 
 	public static void main(String[] args) {
-		int trate; String answer; int tAmount; int totalAmount; int ftAmount;
+		
+		int trate; 
+		String answer; 
+		double tAmount; 
+		double totalAmount=0; 
+		double ftAmount;
+		
 		// TODO Auto-generated method stub
 		//Create Arraylist to store Product information 
 		ArrayList<Product> array = new ArrayList<Product>();
@@ -18,53 +25,61 @@ public class Invoice {
 			Scanner taxr1 = new Scanner (System.in);
 			trate = taxr1.nextInt();
 			
-		do {Product a = new Product();
-			print(""+a.price());
+		do {
+			Product a = new Product();
+			
+			System.out.println("Enter the product price");
+			Scanner price1 = new Scanner (System.in);
+			
+			double price = price1.nextDouble();
+			a.setPrice(price);
+			
 		
-			Product b = new Product();
-			print(b.descr());
+		
+			System.out.println("Enter the product description");
+			Scanner descr1 = new Scanner (System.in);
+			
+			String descr = descr1.nextLine();
+			a.setDescr(descr);
+			
 			
 			System.out.println("Add another Product? y/n");
 			Scanner answer1 = new Scanner (System.in);
 			answer = answer1.nextLine();
 			
-			array.add(a);			
+			array.add(a);
+		
 			
-		} while (answer.equalsIgnoreCase("y"));
+			} while (answer.equalsIgnoreCase("y"));
+		
 		
 		System.out.println("Invoice: ");
 		
-		for (Product item:array);{
+		for (Product item:array){
 			totalAmount += item.getPrice();
-			System.out.println(item.getName() + "   " + item.getPrice());
-			
-
+			System.out.println(item.getDescr() + "   " + item.getPrice());
 		}
+		
 		
 		tAmount = totalAmount * trate/100;
 		ftAmount = totalAmount + tAmount;
 		
-		System.out.println("totalAmount");
-		System.out.println("tax");
-		System.out.println("Total");
+		System.out.println("Tax Rate: "+ trate+"%");
+		System.out.println("Tax Charged: "+ tAmount);
+		System.out.println("Total Amount Due: "+ ftAmount);
 				
 		
 
 		}
-
-	
 	
 
 	private static void print(String s) {
 		// TODO Auto-generated method stub
 		System.out.println(s);
 		
+		
 	}
 
-
-
-	
-	
 		
 	
 }
--- src/invoice/Product.java
+++ src/invoice/Product.java
@@ -3,19 +3,40 @@ package invoice;
 import java.util.Scanner;
 
 public class Product {
-	double price;
-	String descr;
+	private double price;
+	private String descr;
 	
-	public double price(){
-		System.out.println("Enter the product price");
-		Scanner price1 = new Scanner (System.in);
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	
+/*	public void setPrice(double price){
+		this.price=price;	
+	}
+	public double getprice(){
+		//System.out.println("Enter the product price");
+		//Scanner price1 = new Scanner (System.in);
 		return price = price1.nextInt();
 	}
 	
-	public String descr(){
+	public void setdescr(String descr){
+		this.descr=descr;	
+	}
+	public String getdescr(){
 		System.out.println("Enter the product description");
 		Scanner descr1 = new Scanner (System.in);
 		return descr = descr1.nextLine();
-	}
+	}*/
+	
 	
 }
